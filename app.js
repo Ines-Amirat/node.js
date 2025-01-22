6fd7fb8346ad46df8ba743314d46c80a7455f379
Ines Amirat <inesamirat20@gmail.com>
2025-01-22 12:05:00 +0100

    ii

---
+++ app.js
@@ -0,0 +1,29 @@
+const value = 20;
+
+if(value < 20){
+    console.log("hello world");
+}
+else{
+    console.log("next Level");
+}
+
+//Global
+console.log(__dirname);
+console.log(__filename);
+
+
+// setInterval(() => {
+//     console.log("hello world");
+// },1000);
+
+
+
+const name1 ="ahmed";
+const name2 = "mohamed";
+
+
+const sayHello = (name) =>{
+     console.log(name)
+}
+
+sayHello(name1)
